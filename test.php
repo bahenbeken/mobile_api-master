8ea3e24ac035579c0b2872c72bccf98e01688319
tupa.panjaitan <tupa.panjaitan@techno1-consulting.com>
2018-11-16 10:23:40 +0700

    test

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 
 <?php
-	try {
+	//try {
 		
 		//$img = stripslashes($object['photo']);
 		//$img = str_replace('data:image/jpeg;base64,', '', $img);
@@ -9,8 +9,17 @@
 		//$file = __DIR__ . '/backend/assets/photo/customer/test.jpg';
 		//$success = file_put_contents($file, $data);
 		
-		echo($file);
-	} catch(Exception $e) {
-		echo ($e->getMessage());
-	}
+		//echo($file);
+	//} catch(Exception $e) {
+		//echo ($e->getMessage());
+	//}
+	
+	$img = str_replace('data:image/jpeg;base64,', '', 'AAAFBfj42Pj4');
+	$img = str_replace(' ', '+', $img);
+	$data = base64_decode($img);
+	//$file = __DIR__ . '\photo\test.jpg';
+	$file = __DIR__ . '/backend/assets/photo/customer/test.jpg';
+	$success = file_put_contents($file, $data);
+	echo $success;
+	echo $file;
 ?>
